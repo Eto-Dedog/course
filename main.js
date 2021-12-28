189f437a6423df8b05c94a55fc15fe749b74ca9a
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-28 14:13:39 +0300

    Complete lesson04extra-
    Complete lesson04extra-

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-let arr = [222,353,423,164,753,400,264]
-
-console.log('1)');
-
-arr.forEach(em => {
-  if (String(em).search(/(2|4)/) === 0) console.log(em);
-})
-
-console.log('');
-console.log('2)');
-
-function isPrime(n) {
-  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
-  return n > 1;
+const arg = function () {
+  str = prompt('Как называется ваш проект?', 'Калькулятор верстки')
+  if (typeof str != Number) str+=''
+    else {alert('Это не строка!')}
+  if (str.length <= 30) return str
+  str = str.slice(0, 30) + '...'
+  return str
 }
-
-const res = console.log([...Array(101)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n')); 
+console.log(arg().trim());

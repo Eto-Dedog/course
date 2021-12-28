eafaefa6dfe2f4176b554c8663e73a0bd125ecc8
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-28 14:52:11 +0300

    lesson03extra-
    lesson03extra-

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-const arg = function () {
-  str = prompt('Как называется ваш проект?', 'Калькулятор верстки')
-  if (typeof str != Number) str+=''
-    else {alert('Это не строка!')}
-  if (str.length <= 30) return str
-  str = str.slice(0, 30) + '...'
-  return str
-}
-console.log(arg().trim());
+// 1(1)
+
+// let lang = 'ru';
+// if (lang == 'ru') {
+// 	let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// }
+// if (lang == 'en') {
+// 	arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// }
+// console.log(arr);
+
+// 1(2)
+
+// let lang = 'ru';
+// switch (lang) {
+// 	case 'ru':
+// 		let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// 	break;
+// 	case 'en':
+// 		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// 	break;
+// }
+// console.log(arr);
+
+// 1(3)
+
+// let lang = 'ru';
+// let arr = {
+// 	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
+// 	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+// };
+// console.log(arr[lang]);

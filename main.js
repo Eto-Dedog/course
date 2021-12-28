3d02c88f7bba07088599fea3b33507bfa13580aa
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-28 12:41:04 +0300

    Completed lesson 7
    Completed lesson 7

--- main.js
+++ main.js
@@ -12,83 +12,72 @@ const appData = {
   asking: function () {
     appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
     appData.screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные')
-  
-    do {
-      appData.screenPrice = prompt('Сколько будет стоить данная работа?')
-    } while (!isNumber(appData.screenPrice))
-  
+    appData.screenPrice = +prompt('Сколько будет стоить данная работа?', 50000)
     appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-  }
-}
-
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-const getAllServicePrices = function () {
-  let sum = 0
-
-  for (let i = 0; i < 2; i++) {
-    let price = 0
-
-    if (i === 0) {
-      appData.service1 = prompt('Какой дополнительный тип услуги нужен?')
-    } else if (i === 1) {
-      appData.service2 = prompt('Какой дополнительный тип услуги нужен?')
+  },
+  getAllServicePrices: function () {
+    let sum = 0
+  
+    for (let i = 0; i < 2; i++) {
+      let price = 0
+  
+      if (i === 0) {
+        appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'Модальное окно')
+      } else if (i === 1) {
+        appData.service2 = prompt('Какой дополнительный тип услуги нужен?', 'Счётчик')
+      }
+      price = +prompt('Сколько это будет стоить?');
+      sum += +price
     }
+    return sum
+  },
+  getFullPrice: function () {
+    return appData.screenPrice + appData.allServicePrices
+  },
+  getServicePercentPrices: function () {
+    return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+  },
+  getTitle: function () {
+    return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase()
+  },
+  getRollbackMessage: function(price) {
+    if (price >= 30000) {
+      return "Даем скидку в 10%"
+    } else if (price >= 15000 && price < 30000) {
+      return "Даем скидку в 5%"
+    } else if (price >= 0 && price < 15000) {
+      return "Скидка не предусмотрена"
+    } else {
+      return "Что-то пошло не так"
+    }
+  },
+  start: function () {
+    appData.asking()
+    appData.allServicePrices = appData.getAllServicePrices()
+    appData.fullPrice = appData.getFullPrice()
+    appData.serviePercentPrice = appData.getServicePercentPrices()
+    appData.title = appData.getTitle()
+    appData.logger()
+  },
+  logger: function () {
+    console.log("allServicePrices", appData.allServicePrices);
 
-    do {
-      price = prompt('Сколько это будет стоить?');
-    } while (!isNumber(price))
+    console.log(appData.getRollbackMessage(appData.fullPrice));
+    console.log("title",typeof appData.title);
+    console.log("screenPrice",typeof appData.screenPrice);
+    console.log("adaptive",typeof appData.adaptive);
 
-    sum += +price
+    console.log("screens",appData.screens.length);
+    console.log("serviePercentPrice",appData.serviePercentPrice);
 
+    console.log("Стоимость верстки экранов " + appData.screenPrice + " гривен и Стоимость разработки сайта " + appData.fullPrice + " гривен.");
+    console.log(" ");
+    console.log("↓ Свойства и методы appData ↓");
+    console.log(" ");
+    for (var key in appData) {
+      console.log( "Ключ: " + key + " Значение: " + appData[key] );
+    }
   }
-  return sum
 }
 
-const getFullPrice = function () {
-  return appData.screenPrice + appData.allServicePrices
-}
-
-const getServicePercentPrices = function () {
-  return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-}
-
-const getTitle = function () {
-  return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase()
-}
-
-const getRollbackMessage = function(price) {
-  if (price >= 30000) {
-    return "Даем скидку в 10%"
-  } else if (price >= 15000 && price < 30000) {
-    return "Даем скидку в 5%"
-  } else if (price >= 0 && price < 15000) {
-    return "Скидка не предусмотрена"
-  } else {
-    return "Что-то пошло не так"
-  }
-}
-
-appData.asking()
-appData.allServicePrices = getAllServicePrices()
-appData.fullPrice = getFullPrice()
-appData.serviePercentPrice = getServicePercentPrices()
-appData.title = getTitle()
-
-
-console.log(appData.fullPrice);
-console.log(appData.serviePercentPrice);
-
-console.log("allServicePrices", allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(serviePercentPrice);
-
-console.log("Стоимость верстки экранов " + screenPrice + " гривен и Стоимость разработки сайта " + fullPrice + " гривен.");
+appData.start()

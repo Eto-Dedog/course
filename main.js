5ca5cda4d2dcb92774c4b6af867c6930189ec878
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-29 10:19:28 +0300

    Complet lesson06extra
    Complet lesson06extra

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
-min = Math.ceil(1);
-max = Math.floor(50);
-var number = Math.floor(Math.random() * (max - min)) + min
-console.log(number);
+'use strict';
 
-const handlers = [
-  { check: value => value === null, toContinue: false, text: "Игра окончена!" },
-  { check: value => isNaN(parseInt(value)), toContinue: true, text: "Угадай число от 1 до 100" },
-  { check: value => parseInt(value) === number, toContinue: false, text: "Поздравляю, Вы угадали!!!" },
-  { check: value => parseInt(value) > number, toContinue: true, text: "Загаданное число больше" },
-  { check: value => parseInt(value) < number, toContinue: true, text: "Загаданное число меньше" }
-];
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+};
 
-let isGuessing = true;
-while (isGuessing) {
-  const value = prompt("Введи число!");
-  const matchedHandler = handlers.find(handler => handler.check(value));
-  isGuessing = matchedHandler.toContinue;
-  alert(matchedHandler.text);
-}
+const isNum = n => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const start = () => {
+    let rNumber = getRandomInt(100);
+    console.log('rNumber: ', rNumber);
+    let nAttempts = 10;
+    const game = () => {
+        nAttempts--;
+        if (nAttempts < 0) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                start();
+            } else {
+                alert('До свидания');
+                return;
+            }
+        } else {
+            const num = prompt('"Угадай число от 1 до 100"');
+            if (num === null) {
+                alert('До свидания');
+                return;
+            }
+            if (isNum(num)) {
+                const realNum = +num;
+                if (realNum > rNumber) {
+                    alert(`Загаданное число меньше, осталось попыток: ${nAttempts}`);
+                    game();
+                } else if (realNum < rNumber) {
+                    alert(`Загаданное число больше, осталось попыток: ${nAttempts}`);
+                    game();
+                } else {
+                    if (confirm('Вы угадали! Хотели сыграть ещё?')) {
+                        start();
+                    } else {
+                        alert('До свидания');
+                        return;
+                    }
+                }
+            } else {
+                alert('Введите число');
+                game();
+            }
+        }
+    };
+    console.dir(game);
+    game();
+};
+
+start();

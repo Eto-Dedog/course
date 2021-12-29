fcefc3240124902d9956c952769427b2077edb6c
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-29 10:16:10 +0300

    Complet lesson06
    Complet lesson06

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var number = Math.floor(Math.random() * (max - min)) + min
 console.log(number);
 
 const handlers = [
-  { check: value => value === null, toContinue: false, text: "Игра окончена" },
+  { check: value => value === null, toContinue: false, text: "Игра окончена!" },
   { check: value => isNaN(parseInt(value)), toContinue: true, text: "Угадай число от 1 до 100" },
   { check: value => parseInt(value) === number, toContinue: false, text: "Поздравляю, Вы угадали!!!" },
   { check: value => parseInt(value) > number, toContinue: true, text: "Загаданное число больше" },

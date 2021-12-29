9336e68d05dc7fe3adede7f8f6044798cf09bae5
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-12-29 10:02:10 +0300

    Complet lesson04extra
    Complet lesson04extra

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const arg = function () {
-  str = prompt('Как называется ваш проект?', 'Калькулятор верстки')
-  if (typeof str != Number) str+=''
-    else {alert('Это не строка!')}
-  if (str.length <= 30) return str
-  str = str.slice(0, 30) + '...'
-  return str
-}
-console.log(arg().trim());
+'use strict';
+
+const max = (arg) => {
+    let res = 'Ожидается появление строки...';
+    if (typeof arg === "string") {
+        const maxLength = 30;
+        const str = arg.trim();
+        if (str.length > maxLength) {
+            res = str.substr(0,maxLength) + '...';
+        } else {
+            res = str;
+        }
+    }
+    return res;
+};
+
+console.log('max(): ', max(5));
+console.log('max(): ', max('    Строка менее 30 символов     '));
+console.log('max(): ', max('    Строка более 30 символов - уауауауауауауауауауауауауауауауауауауауауауауауауауауауауауа!'));

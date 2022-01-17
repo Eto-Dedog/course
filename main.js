0d25cc5da9c106e521734df2c91542a33fe18765
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-01-17 12:08:11 +0300

    Complet Lesson16
    Complet Lesson16

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-min = Math.ceil(1);
-max = Math.floor(50);
-var number = Math.floor(Math.random() * (max - min)) + min
-console.log(number);
+"use strict";
 
-const handlers = [
-  { check: value => value === null, toContinue: false, text: "Игра окончена" },
-  { check: value => isNaN(parseInt(value)), toContinue: true, text: "Угадай число от 1 до 100" },
-  { check: value => parseInt(value) === number, toContinue: false, text: "Поздравляю, Вы угадали!!!" },
-  { check: value => parseInt(value) > number, toContinue: true, text: "Загаданное число больше" },
-  { check: value => parseInt(value) < number, toContinue: true, text: "Загаданное число меньше" }
-];
-
-let isGuessing = true;
-while (isGuessing) {
-  const value = prompt("Введи число!");
-  const matchedHandler = handlers.find(handler => handler.check(value));
-  isGuessing = matchedHandler.toContinue;
-  alert(matchedHandler.text);
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
+  }
 }
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const test = new Second();
+
+test.hello();

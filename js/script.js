9be388dcc6b8cda6c5f71ce4e48e9683218f6368
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-09 11:29:03 +0300

    Complet Lesson09
    Complet Lesson09

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 title = document.getElementsByTagName('h1')
-console.log(title);
-button = document.getElementsByClassName('handler_btn')
+console.log(title[0]);
+button = document.getElementsByClassName('handler_btn')[0]
 console.log(button);
 buttonTiming = document.querySelector('.screen-btn')
 console.log(buttonTiming);
@@ -13,5 +13,5 @@ span = document.querySelector('.rollback .range-value')
 console.log(span);
 input = document.getElementsByClassName('total-input')
 console.log(input);
-let = document.querySelectorAll('.screen')
-console.log(let);
+let screen = document.querySelectorAll('.screen')
+console.log(screen);
